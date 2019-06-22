c80005640dab4b5fd951dc18dca0b3dec25f40e2
Jhonatan de Souza <jdsjhonatan@hotmail.com>
2019-06-22 12:07:31 -0300

    Translating all items in home component to english

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -27,93 +27,85 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.page.actionBarHidden = true;
   }
-  calcularImpostos() {
-    this.calcularImpostosSobFaturamento();
-    this.calcularInss();
-    this.calcularInrf();
-    this.calcularTotalImpostosAPagar();
-    this.calcularLucroEmpresa();
+  calculateTaxes() {
+    this.calculateTaxesOnProfit();
+    this.calculateInssTaxes();
+    this.calculateInrfTaxes();
+    this.calculateTotalTaxesToPay();
+    this.calculateCompanyProfit();
   }
   
-  calcularImpostosSobFaturamento() {
-    //this.impostoEmpresaAPagar = this.faturamentoPreImposto * (this.percentualImposto / 100);
+  calculateTaxesOnProfit() {
     this.companyInfo.companyProfitTax = this.companyInfo.profitBeforeTax  * (this.companyInfo.companyIncomePercentTax / 100);
   }
   // Calcula INSS - Previdência Social
-  calcularInss() {
-    //const percentualInssSobreSalario = 0.11;
+  calculateInssTaxes() {
     const inssTaxOnWage = 0.11; // Percentual Inss sobre Salário
-    //this.salarioProLabore = this.faturamentoPreImposto * (this.percentualProLabore / 100);
     this.companyInfo.associateWage = this.companyInfo.profitBeforeTax * (this.companyInfo.associateWagePercent / 100);
-    //this.inssAPagar = Math.round(this.salarioProLabore * percentualInssSobreSalario)
     this.companyInfo.inssTax = Math.round(this.companyInfo.associateWage * inssTaxOnWage);
   }
 
   //Calcular Imposto de Renda Pessoa Física
-  calcularInrf() {
-    const salarioLiquido = this.companyInfo.associateWage - this.companyInfo.inssTax;
-    const valorNaoTributavel = 1903.98;
-    const minFaixa1 = 1903.99;
-    const maxFaixa1 = 2826.65;
-    const minFaixa2 = 2826.66;
-    const maxFaixa2 = 3751.05;
-    const minFaixa3 = 3751.06;
-    const maxFaixa3 = 4664.68;
-    const minFaixa4 = 4664.69;
-    const deducaoFaixa1 = 142.80;
-    const deducaoFaixa2 = 354.80;
-    const deducaoFaixa3 = 636.13;
-    const deducaoFaixa4 = 869.36;
-    const percentualFaixa1 = 7.5;
-    const percentualFaixa2 = 15;
-    const percentualFaixa3 = 22.5;
-    const percentualFaixa4 = 27.5;
+  calculateInrfTaxes() {
+    const wage = this.companyInfo.associateWage - this.companyInfo.inssTax;
+    const nonTaxableWage = 1903.98;
+    const range1Min = 1903.99;
+    const range1Max = 2826.65;
+    const range2Min = 2826.66;
+    const range2Max = 3751.05;
+    const range3Min = 3751.06;
+    const range3Max = 4664.68;
+    const range4Min = 4664.69;
+    const range1Deduction = 142.80;
+    const range2Deduction = 354.80;
+    const range3Deduction = 636.13;
+    const range4Deduction = 869.36;
+    const range1Percent = 7.5;
+    const range2Percent = 15;
+    const range3Percent = 22.5;
+    const range4Percent = 27.5;
 
-    if (salarioLiquido <= valorNaoTributavel) {
+    if (wage <= nonTaxableWage) {
       this.companyInfo.irpfTax = 0;
     }
-    if (enquadraFaixa1()) {
-      this.companyInfo.irpfTax = Math.round((salarioLiquido * (percentualFaixa1 / 100)) - deducaoFaixa1);
+    if (isWithinRange1()) {
+      this.companyInfo.irpfTax = Math.round((wage * (range1Percent / 100)) - range1Deduction);
     }
 
-    if (enquadraFaixa2()) {
-      this.companyInfo.irpfTax = Math.round((salarioLiquido * (percentualFaixa2 / 100)) - deducaoFaixa2);
+    if (isWithinRange2()) {
+      this.companyInfo.irpfTax = Math.round((wage * (range2Percent / 100)) - range2Deduction);
     }
 
-    if (enquadraFaixa3()) {
-      this.companyInfo.irpfTax = Math.round((salarioLiquido * (percentualFaixa3 / 100)) - deducaoFaixa3);
+    if (isWithinRange3()) {
+      this.companyInfo.irpfTax = Math.round((wage * (range3Percent / 100)) - range3Deduction);
     }
 
-    if (enquadraFaixa4()) {
-      this.companyInfo.irpfTax = Math.round((salarioLiquido * (percentualFaixa4 / 100)) - deducaoFaixa4);
+    if (isWithinRange4()) {
+      this.companyInfo.irpfTax = Math.round((wage * (range4Percent / 100)) - range4Deduction);
     }
 
-    function enquadraFaixa1() {
-      return salarioLiquido >= minFaixa1 && salarioLiquido <= maxFaixa1;
+    function isWithinRange1() {
+      return wage >= range1Min && wage <= range1Max;
     }
-    function enquadraFaixa2() {
-      return salarioLiquido >= minFaixa2 && salarioLiquido <= maxFaixa2;
+    function isWithinRange2() {
+      return wage >= range2Min && wage <= range2Max;
     }
-    function enquadraFaixa3() {
-      return salarioLiquido >= minFaixa3 && salarioLiquido <= maxFaixa3;
+    function isWithinRange3() {
+      return wage >= range3Min && wage <= range3Max;
     }
-    function enquadraFaixa4() {
-      return salarioLiquido >= minFaixa4;
+    function isWithinRange4() {
+      return wage >= range4Min;
     }
 
-    console.log('finalizando  calculo Inrf');
   }
 
   // Calcula o Total de Impostos a Pagar
-  calcularTotalImpostosAPagar() {
-    //this.totalAPagar = this.inssAPagar + this.irpfAPagar + this.impostoEmpresaAPagar;
+  calculateTotalTaxesToPay() {
     this.companyInfo.totalTaxes = this.companyInfo.inssTax + this.companyInfo.irpfTax + this.companyInfo.companyProfitTax;
   }
   //Calcula quanto a empresa irá lucrar após pagar impostos
-  calcularLucroEmpresa() {
-    //this.quantoVouLucrar = this.faturamentoPreImposto - this.totalAPagar;
+  calculateCompanyProfit() {
     this.companyInfo.totalProfit = this.companyInfo.profitBeforeTax - this.companyInfo.totalTaxes;
-    //this.quantoVouLucrar = 'R$' + this.quantoVouLucrar;
     this.companyInfo.totalProfit = 'R$' + this.companyInfo.totalProfit;
   }
 }
